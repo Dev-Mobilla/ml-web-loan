0674a681fdc16a6119ec64f6b600d408ade1fd34
Charebel Bejoc <chaeybejoc@gmail.com>
2023-08-29 16:53:11 +0800

    create dashboard page

--- src/components/layout/HeaderComponent.jsx
+++ src/components/layout/HeaderComponent.jsx
@@ -6,6 +6,18 @@ const HeaderComponent = () => {
     <header className="header">
             <div className="overlap-group">
               <div className="bannertitle">Loan Marketplace</div>
+              <div className="download-btn">
+                <div className="overlap-3">
+                  <div className="dlcard" />
+                  <div className="dltxtt">Download on</div>
+                  <div className="dltxt">Google Play</div>
+                  <img
+                    className="dl-btn"
+                    alt="Dl btn"
+                    src="https://anima-uploads.s3.amazonaws.com/projects/64e41d552340cba66b90f01a/releases/64e41e67e1c2a81b98b3c871/img/dl-btn@2x.png"
+                  />
+                </div>
+              </div>
               <div className="existing-loan-btn">
                 <div className="overlap-group-2">
                   <div className="overlap-2">
@@ -16,18 +28,6 @@ const HeaderComponent = () => {
                     className="ml-btn"
                     alt="Ml btn"
                     src="https://anima-uploads.s3.amazonaws.com/projects/64e41d552340cba66b90f01a/releases/64e492714fd92dc35e55a22f/img/ml-btn-5@2x.png"
-                  />
-                </div>
-              </div>
-              <div className="download-btn">
-                <div className="overlap-3">
-                  <div className="dlcard" />
-                  <div className="dltxtt">Download on</div>
-                  <div className="dltxt">Google Play</div>
-                  <img
-                    className="dl-btn"
-                    alt="Dl btn"
-                    src="https://anima-uploads.s3.amazonaws.com/projects/64e41d552340cba66b90f01a/releases/64e41e67e1c2a81b98b3c871/img/dl-btn@2x.png"
                   />
                 </div>
               </div>
