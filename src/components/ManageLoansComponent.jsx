e9c2ec53ab15c70e7ceebcfd20a91f3908a0b508
dev-mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2023-08-30 16:27:28 +0800

    manage-loan-responsive-page

--- src/components/ManageLoansComponent.jsx
+++ src/components/ManageLoansComponent.jsx
@@ -8,23 +8,6 @@ const ManageLoanComponent = () => {
       <div className="div">
       <TopbarComponent/>
       <CustomHeader title="Manage Existing Loans"/>
-        {/* <footer className="footer">
-          <img
-            className="reserved"
-            alt="Reserved"
-            src="https://anima-uploads.s3.amazonaws.com/projects/64e41d552340cba66b90f01a/releases/64e41e67e1c2a81b98b3c871/img/screenshot-2023-05-31-101930-1.png"
-          />
-          <img
-            className="aboutus"
-            alt="Aboutus"
-            src="https://anima-uploads.s3.amazonaws.com/projects/64e41d552340cba66b90f01a/releases/64e41e67e1c2a81b98b3c871/img/screenshot-2023-05-31-102020-1@2x.png"
-          />
-          <img
-            className="privacynotice"
-            alt="Privacynotice"
-            src="https://anima-uploads.s3.amazonaws.com/projects/64e41d552340cba66b90f01a/releases/64e41e67e1c2a81b98b3c871/img/screenshot-2023-05-31-102043-1@2x.png"
-          />
-        </footer> */}
         <div className="body-bg">
           <div className="prev-btn">
             <CustomPrevBtn/>
