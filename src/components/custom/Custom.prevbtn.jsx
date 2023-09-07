423236bf5a5521a381d290ba8c79edae5fb73431
chemuel <chemuelgodes@gmail.com>
2023-09-07 16:11:14 +0800

    QuickPrevious

--- src/components/custom/Custom.prevbtn.jsx
+++ src/components/custom/Custom.prevbtn.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { PreviousBTNComponent } from '../index';
 
 const CustomPrevBtn = () => {
   return (
     <div className="custom-prevbtn prevpagebtn">
+      <PreviousBTNComponent route="/manage-loans">
       <img
         className="arrow"
         alt="Arrow"
         src="https://anima-uploads.s3.amazonaws.com/projects/64e41d552340cba66b90f01a/releases/64e5956d9f04124e98dc5ac7/img/arrow-2@2x.png"
       />
+      </PreviousBTNComponent>
     </div>
   );
 };
