de90c831d1f9f4589f4ca2b577e90cc34b4da514
dev-mobilla <101626428+Dev-Mobilla@users.noreply.github.com>
2023-10-03 12:21:02 +0800

    api-integration/symph-api-add-logger

--- server/controller/symph.controller.js
+++ server/controller/symph.controller.js
@@ -1,8 +1,6 @@
 const RefundBillsPayment = async () => {
     try {
         console.log(process.env.API_SYMPH_BASE_URL);
-        console.log(process.env.SYMPH_API_KEY);
-        console.log(process.env.SYMPH_SECRET_KEY);
     } catch (error) {
         console.log(error);
     }
